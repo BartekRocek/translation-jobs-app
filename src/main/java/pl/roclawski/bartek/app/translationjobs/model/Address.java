6eca89bad3ad8c958cf7a379122b71551b9440b2
Bartek <b.roclawski@gmail.com>
2023-02-24 10:04:10 +0100

    Added: missing models

---
+++ src/main/java/pl/roclawski/bartek/app/translationjobs/model/Address.java
@@ -0,0 +1,22 @@
+package pl.roclawski.bartek.app.translationjobs.model;
+
+public class Address {
+
+
+    private String street;
+    private String number;
+    private String city;
+    private String postCode;
+    private String telephone;
+    private String email;
+
+    public Address(String street, String number, String city, String postCode, String telephone, String email) {
+        this.street = street;
+        this.number = number;
+        this.city = street;
+        this.postCode =
+        this.telephone = telephone;
+        this.email = email;
+    }
+
+}
