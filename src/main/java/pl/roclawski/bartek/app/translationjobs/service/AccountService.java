e5cff773720fc65e5b7bc3e2a550d180ee192720
Bartek <b.roclawski@gmail.com>
2023-03-10 11:45:33 +0100

    added: spring app main class, business logic AccountModel

---
+++ src/main/java/pl/roclawski/bartek/app/translationjobs/service/AccountService.java
@@ -0,0 +1,27 @@
+package pl.roclawski.bartek.app.translationjobs.service;
+
+import pl.roclawski.bartek.app.translationjobs.model.Account;
+import pl.roclawski.bartek.app.translationjobs.repository.AccountEntity;
+import pl.roclawski.bartek.app.translationjobs.repository.AccountRepository;
+import pl.roclawski.bartek.app.translationjobs.repository.mapper.AccountMapper;
+
+public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    private final AccountMapper accountMapper;
+
+    public AccountService(AccountRepository accountRepository, AccountMapper accountMapper) {
+        this.accountRepository = accountRepository;
+        this.accountMapper = accountMapper;
+    }
+
+    public Account account(Account account) {
+        AccountEntity accountEntity = accountMapper.from(account);
+        AccountEntity savedAccountEntity = accountRepository.save(accountEntity);
+        Account mappedAccount = accountMapper.from(savedAccountEntity);
+
+        return mappedAccount;
+    }
+
+}
