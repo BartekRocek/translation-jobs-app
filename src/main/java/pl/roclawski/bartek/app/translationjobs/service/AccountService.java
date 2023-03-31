b653d9c79cdc17578f2375355b645b0197dc8498
Bartek <b.roclawski@gmail.com>
2023-03-31 13:34:55 +0200

    added: Get and Post methods, create() along with account create form updated: access to h2-console and db url

--- src/main/java/pl/roclawski/bartek/app/translationjobs/service/AccountService.java
+++ src/main/java/pl/roclawski/bartek/app/translationjobs/service/AccountService.java
@@ -1,14 +1,18 @@
 package pl.roclawski.bartek.app.translationjobs.service;
 
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import pl.roclawski.bartek.app.translationjobs.controller.AccountController;
 import pl.roclawski.bartek.app.translationjobs.model.Account;
 import pl.roclawski.bartek.app.translationjobs.repository.AccountEntity;
 import pl.roclawski.bartek.app.translationjobs.repository.AccountRepository;
 import pl.roclawski.bartek.app.translationjobs.repository.mapper.AccountMapper;
 
+import java.util.logging.Logger;
+
 @Service
 public class AccountService {
+
+    public static final Logger LOGGER = Logger.getLogger(AccountService.class.getName());
 
     private final AccountRepository accountRepository;
 
@@ -19,11 +23,14 @@ public class AccountService {
         this.accountMapper = accountMapper;
     }
 
-    public Account account(Account account) {
+    public Account create(Account account) {
+        LOGGER.info("create(" + account + ")");
+
         AccountEntity accountEntity = accountMapper.from(account);
         AccountEntity savedAccountEntity = accountRepository.save(accountEntity);
         Account mappedAccount = accountMapper.from(savedAccountEntity);
 
+        LOGGER.info("create(...) = " + mappedAccount);
         return mappedAccount;
     }
 
